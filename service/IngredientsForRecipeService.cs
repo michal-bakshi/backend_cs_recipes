d589c29cd0ea9c0c75f555e8042d3e722414cdaa
Michal Bakshi <michalb5980@gmail.com>
2025-11-26 23:06:24 +0200

    cs backend for recipes app

---
+++ service/IngredientsForRecipeService.cs
@@ -0,0 +1,32 @@
+﻿using Core.Reposetories;
+using Core.Services;
+using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class IngredientsForRecipeService:IIngredientsForRecipeService
+    {
+        public readonly IIngredientsForRecipeReposetory _IngredientsForResipeRepository;
+
+        public IngredientsForRecipeService(IIngredientsForRecipeReposetory ingredientsForRecipeRepository)
+        {
+            _IngredientsForResipeRepository = ingredientsForRecipeRepository;
+        }
+
+        public List<IngredientsForRecipe> AddIngredient(int id, Dictionary<int, string> intIngredient)
+        {
+           return _IngredientsForResipeRepository.AddIngredient(id, intIngredient);
+        }
+
+        public List<IngredientsForRecipe> GetIngredientRecipe(int id)
+        {
+            return _IngredientsForResipeRepository.GetIngredientRecipe(id);
+        }
+    }
+}
+
---
+++ service/UserSevice.cs
@@ -0,0 +1,28 @@
+﻿using Core.Reposetories;
+using Core.Services;
+using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class UserSevice : IUserService
+    {
+        private readonly IUserReposetory _userRepo;
+        public UserSevice(IUserReposetory userRepo) {
+            _userRepo = userRepo;
+        }
+        public User AddUser(User user)
+        {
+            return _userRepo.AddUser(user);
+        }
+
+        public User GetUserBy(string email, string password)
+        {
+            return _userRepo.GetUserBy(email, password);
+        }
+    }
+}
